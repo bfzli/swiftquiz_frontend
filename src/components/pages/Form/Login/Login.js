6ecc1c40a3c6e25d7d0d14c45d60c527f8fba29b
Benjamin Fazli <bfzli@hotmail.com>
2021-11-03 13:44:47 +0100

    New Update Laurat and Benjamin

--- src/components/pages/Form/Login/Login.js
+++ src/components/pages/Form/Login/Login.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState} from 'react'
 import facebook from '../../../../assets/images/facebook.png'
 import google from '../../../../assets/images/google.png'
 import github from '../../../../assets/images/github.png'
@@ -9,55 +9,53 @@ import ValidationLogin from '../../../../utils/ValidationLogin'
 import axios, { Axios } from 'axios'
 
 
-function Login() {
+export default function Login() {
 
-    const [details,setDetails] = useState({username: "", password: ""});
-    const [errors,setErrors] = useState({});
-    const [backError,setBackError] = useState(null);
-    
-    const LoginHandler = (e) =>{
+    const [details, setDetails] = useState({ username: "", password: "" });
+    const [errors, setErrors] = useState({});
+    const [backError, setBackError] = useState(null);
+
+    const LoginHandler = (e) => {
         e.preventDefault();
         setErrors(ValidationLogin(details));
-        
+
         const logindata = {
             username: details.username,
             password: details.password
         };
-        
-        axios.post("http://localhost:5000/api/users/login-user",logindata).then((res)=>{
-            if(res.status == 403){
-                    throw Error ("Incorrect Password");
-            }else if(res.status == 404){
-                    throw Error ("Username is not found");
+
+        axios.post("http://localhost:5000/api/users/login-user", logindata).then((res) => {
+            if (res.status == 403) {
+                throw Error("Incorrect Password");
+            } else if (res.status == 404) {
+                throw Error("Username is not found");
             }
             console.log(res);
-        }).then((data)=>{
+        }).then((data) => {
             setBackError(null);
-        }).catch((err)=>{
-                setBackError(err.response.data.message);
-            })
-        }
+        }).catch((err) => {
+            setBackError(err.response.data.message);
+        })
+    }
 
     return (
-                    <div className="form_container sign_in_container">
-                        <form name="forma2" onSubmit={LoginHandler}>
-                            {backError && <div className="BackError" style={{marginLeft:'115px'}}>{backError}</div>}
-                            <h1>Sign in</h1>
-                            <div className="social_container">
-                                <a className="social"><img src={facebook} alt="" width="40px" height="40"/></a>
-                                <a className="social"><img src={google} alt="" width="40px" height="40px"/></a>
-                                <a className="social"><img src={github} alt="" width="40px" height="40px"/></a>
-                            </div>
-                            <span>or use your account</span>
-                            <input type="text" className="inputat" placeholder="Username" name="username" id="email" onChange={(e) => setDetails({...details, username: e.target.value})} value={details.username} />                    
-                            {errors.username && <p className="error">{errors.username}</p>}                            
-                            <input type="password" className="inputat" placeholder="Password" name="password"  id="passs" onChange={(e) => setDetails({...details, password: e.target.value})} value={details.password} />
-                            {errors.password && <p className="error">{errors.password}</p>}                            
-                            <a href="">Forgot your password?</a>
-                            <input type="submit"className="butonat" id="sIn" value="Sign In" name="submit"/>
-                        </form> 
-                    </div>    
+        <div className="form_container sign_in_container">
+            <form name="forma2" onSubmit={LoginHandler}>
+                {backError && <div className="BackError" style={{ marginLeft: '115px' }}>{backError}</div>}
+                <h1>Sign in</h1>
+                <div className="social_container">
+                    <a className="social"><img src={facebook} alt="" width="40px" height="40" /></a>
+                    <a className="social"><img src={google} alt="" width="40px" height="40px" /></a>
+                    <a className="social"><img src={github} alt="" width="40px" height="40px" /></a>
+                </div>
+                <span>or use your account</span>
+                <input type="text" className="inputat" placeholder="Username" name="username" id="email" onChange={(e) => setDetails({ ...details, username: e.target.value })} value={details.username} />
+                {errors.username && <p className="error">{errors.username}</p>}
+                <input type="password" className="inputat" placeholder="Password" name="password" id="passs" onChange={(e) => setDetails({ ...details, password: e.target.value })} value={details.password} />
+                {errors.password && <p className="error">{errors.password}</p>}
+                <a href="">Forgot your password?</a>
+                <input type="submit" className="butonat" id="sIn" value="Sign In" name="submit" />
+            </form>
+        </div>
     );
 }
-
-export default Login
