324aa4bb8643f9b59cf4cbeb27a48ff34451780b
adnankas <63065438+adnankas@users.noreply.github.com>
2021-11-03 16:30:05 +0100

    Fix conflicts after merging with other branches

--- src/components/pages/Form/Login/Login.js
+++ src/components/pages/Form/Login/Login.js
@@ -1,9 +1,10 @@
 import React, {useState, useEffect} from 'react'
-import facebook from '../../../../assets/images/facebook.png'
-import google from '../../../../assets/images/google.png'
-import github from '../../../../assets/images/github.png'
+import facebook from '../../../../assets/images/social/facebook.png'
+import google from '../../../../assets/images/social/google.png'
+import github from '../../../../assets/images/social/github.png'
 import './Login.scss'
 import '../shared/LoginRegister.scss'
+import * as styles from '../Globals.module.scss'
 
 import ValidationLogin from '../../../../utils/ValidationLogin'
 import axios, { Axios } from 'axios'
@@ -40,20 +41,20 @@ function Login() {
 
     return (
                     <div className="form_container sign_in_container">
-                        <form name="forma2" onSubmit={LoginHandler}>
+                        <form className={styles.form} onSubmit={LoginHandler}>
                             {backError && <div className="BackError" style={{marginLeft:'115px'}}>{backError}</div>}
-                            <h1>Sign in</h1>
+                            <h1 className={styles.h1}>Sign in</h1>
                             <div className="social_container">
-                                <a className="social"><img src={facebook} alt="" width="40px" height="40"/></a>
-                                <a className="social"><img src={google} alt="" width="40px" height="40px"/></a>
-                                <a className="social"><img src={github} alt="" width="40px" height="40px"/></a>
+                                <a className={styles.a}><img src={facebook} alt="" width="40px" height="40"/></a>
+                                <a className={styles.a}><img src={google} alt="" width="40px" height="40px"/></a>
+                                <a className={styles.a}><img src={github} alt="" width="40px" height="40px"/></a>
                             </div>
-                            <span>or use your account</span>
+                            <span className={styles.span}>or use your account</span>
                             <input type="text" className="inputat" placeholder="Username" name="username" id="email" onChange={(e) => setDetails({...details, username: e.target.value})} value={details.username} />                    
                             {errors.username && <p className="error">{errors.username}</p>}                            
                             <input type="password" className="inputat" placeholder="Password" name="password"  id="passs" onChange={(e) => setDetails({...details, password: e.target.value})} value={details.password} />
                             {errors.password && <p className="error">{errors.password}</p>}                            
-                            <a href="">Forgot your password?</a>
+                            <a className={styles.a} href="">Forgot your password?</a>
                             <input type="submit"className="butonat" id="sIn" value="Sign In" name="submit"/>
                         </form> 
                     </div>    
--- src/pages/FormView.js
+++ src/pages/FormView.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Form from '../components/pages/Form/Form';
-import '../components/shared/Website/FormGeneral.scss'
 
 function FormView() {
     return (
