7dabe2057f9c9103aaf553aa87ff7b0e3da6f5ec
Benjamin Fazli <bfzli@hotmail.com>
2021-11-30 13:44:45 +0100

    New Vercel Fix

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,18 +1,15 @@
 import { Helmet } from 'react-helmet'
 import ErrorView from '../components/pages/Error/Error'
-import Header from '../components/shared/Header/Header'
+import Layout from './Layout'
 
 export default function Error() {
     return (
-        <>
+        <Layout>
             <Helmet>
                 <title>404 Not Found - Swiftquiz</title>
                 <meta name="description" content="Ops something went wrong with the page, if you think this is a mistake contact the team." />
             </Helmet>
-           <div id="globl">
-           <Header />
-           </div>
             <ErrorView />
-        </>
+        </Layout>
     )
 }
