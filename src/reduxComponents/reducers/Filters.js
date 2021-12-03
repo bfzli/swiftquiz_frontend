a902c762cffc65d4dee80b5f658492dfc8de68cc
Laurat <l4tt1haga@gmail.com>
2021-12-03 09:33:19 +0100

    Created Category Filter

--- src/reduxComponents/reducers/Filters.js
+++ src/reduxComponents/reducers/Filters.js
@@ -1,28 +1,36 @@
+import * as CONST from '../constants/index';
+
 const initialState = {
-    text: '',
-    difficulty: 1
+	text: '',
+	difficulty: 1,
+	category: ''
 };
 
-
 const filterReducer = (state = initialState, action) => {
-    const { payload } = action;
-    
-    switch(action.type){
-        case 'SET_TEXT_FILTER':
-            return {
-                ...state,
-                text: payload
-            }
+	const { payload } = action;
 
-        case 'SORT_BY_DIFFICULTY':
-            return {
-                ...state,
-                difficulty: payload
-            }
+	switch (action.type) {
+		case CONST.SET_TEXT_FILTER:
+			return {
+				...state,
+				text: payload
+			};
 
-        default:
-            return state;
-    };
+		case CONST.SORT_BY_DIFFICULTY:
+			return {
+				...state,
+				difficulty: payload
+			};
+
+		case CONST.SET_CATEGORY_FILTER:
+			return {
+				...state,
+				category: payload
+			};
+
+		default:
+			return state;
+	}
 };
 
 export default filterReducer;
