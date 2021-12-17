1a1706ce57fd45a527cec1a8611efbd7eeba02f8
Benjamin Fazli <bfzli@hotmail.com>
2021-12-17 08:24:04 +0100

    New Routes and Permissons fixes

--- src/Routing.js
+++ src/Routing.js
@@ -30,6 +30,8 @@ import { useTranslation } from 'react-i18next';
 import Support from './pages/Dashboard/Support';
 import Help from './pages/Help';
 import Homev2 from './pages/Homev2'
+import Redirect from './utils/Redirect'
+import AdminPass from './utils/AdminPass'
 
 export default function Routing() {
   const dispatch = useDispatch();
@@ -102,7 +104,6 @@ export default function Routing() {
 					<Route path="/play" element={<Play />} />
 					<Route path="/dashboard/quizzes/add-quiz" element={<Quiz />} />
 					<Route path="/dashboard/quizzes/edit-quiz/:id" element={<EditQuiz />} />
-					<Route path="/dashboard/admin" element={<AdminPanel />} />
 					<Route path="/dashboard/community" element={<Community />} />
 					<Route path="/dashboard/profile/edit" element={<EditProfile />} />
 					<Route path="/dashboard/profile/:username" element={<UserProfile />} />
@@ -113,11 +114,16 @@ export default function Routing() {
 					<Route path="/dashboard/store" element={<Store />} />
 					<Route path="/dashboard/messenger" element={<Support />} />
 					<Route path="/dashboard/help" element={<Help />} />
-					<Route path="/dashboard"></Route> {/* should be fixed to redirect*/}
+					<Route path="/dashboard" element={<Redirect to="/dashboard/quizzes"/>} />
 					<Route path="/dashboard/auth" element={<Dashauth />} />
 					<Route path="/*" element={<Error />} />
 					<Route path="/dashboard/*" element={<Error />} />
 				</Route>
+
+        <Route exact path='/' element={<AdminPass />}>
+          <Route path="/dashboard/admin" element={<AdminPanel />} /> 
+        </Route>
+
 			</Switch>
 		</Wrapper>
 	);
