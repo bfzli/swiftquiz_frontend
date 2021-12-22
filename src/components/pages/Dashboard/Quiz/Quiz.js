295dbe722bcfd520abad219014ae691aa6af7f4b
Benjamin Fazli <bfzli@hotmail.com>
2021-12-22 20:38:45 +0100

    fixed quiz page

--- src/components/pages/Dashboard/Quiz/Quiz.js
+++ src/components/pages/Dashboard/Quiz/Quiz.js
@@ -1,10 +1,11 @@
-
 import * as styles from './Quiz.module.scss';
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { createQuiz } from '../../../../reduxComponents/actions/Questions';
 import { useTranslation } from 'react-i18next';
 import Absoloader from '../../../shared/Loaders/Absoloader'
+
+export default function Quiz() {
   const dispatch = useDispatch();
   const [sidebarView, setSidebarView] = useState('question_operations');
   const [mode, setMode] = useState('add');
@@ -18,7 +19,19 @@ import Absoloader from '../../../shared/Loaders/Absoloader'
   const [quizImage, setQuizImage] = useState();
   const [stopTyping, setStopTyping] = useState(false);
   const [isAdded, setIsAdded] = useState(false);
+
   const test = t('quiz.quizbuilder');
+
+  function isEmpty(obj) {
+    for (var prop in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+        return false;
+      }
+    }
+
+    return JSON.stringify(obj) === JSON.stringify({});
+  }
+
   function checkValidation() {
     if (quiz.title === '')
       errors.title = 'The Quiz must have a title to be published.';
@@ -27,6 +40,7 @@ import Absoloader from '../../../shared/Loaders/Absoloader'
     // if (quiz.thumbnail === '') errors.thumbnail = 'A Quiz thumbnail is required to begin with.';
     if (quiz.questions.length === 0)
       errors.no_questions = 'You should add at least 1 question with anwsers.';
+
     quiz.questions.map((item) => {
       if (item.question === '')
         errors.question = 'All questions should be filled';
@@ -58,6 +72,9 @@ import Absoloader from '../../../shared/Loaders/Absoloader'
       setTimeout(function () {
         setNotification(false);
       }, 5000);
+    }
+  }
+
   const [quiz, setQuiz] = useState({
     title: '',
     description: '',
@@ -68,6 +85,7 @@ import Absoloader from '../../../shared/Loaders/Absoloader'
     questions: [],
     thumbnail: '',
   });
+
   const [fields, updateFields] = useState({
     quiz_id: Math.floor(100000 + Math.random() * 900000),
     question: '',
@@ -77,6 +95,7 @@ import Absoloader from '../../../shared/Loaders/Absoloader'
     answer4: '',
     isCorrect: '',
   });
+
   function handleCorrectAnwser(which) {
     if (which === 'answer1') {
       setTrueQ('anwser1');
@@ -92,11 +111,13 @@ import Absoloader from '../../../shared/Loaders/Absoloader'
       setTrue('isCorrect', 'answer4');
     }
   }
+
   const addQuestion = (question) => {
     setQuestionList([...questionList, question]);
     setQuiz({ ...quiz, questions: [...questionList, question] });
     if (mode === 'add') {
       updateFields({
+        quiz_id: Math.floor(100000 + Math.random() * 900000),
         question: '',
         answer1: '',
         answer2: '',
@@ -104,19 +125,24 @@ import Absoloader from '../../../shared/Loaders/Absoloader'
         answer4: '',
         isCorrect: '',
       });
+    }
     setTrueQ('none');
   };
+
   useEffect(() => {
     window.onkeydown = function (event) {
       var key = event.keyCode || event.charCode;
       if (key == 8) setStopTyping(false);
     };
+
     if (fields.question.length > 99) setStopTyping(true);
   }, [fields]);
+
   const editQuestion = (question) => {
     setMode('edit');
     setEdited(question);
   };
+
   const deleteQuestion = (question) => {
     const newQuestions = questionList.filter(
       (q) => q.quiz_id !== question.quiz_id
@@ -124,8 +150,10 @@ import Absoloader from '../../../shared/Loaders/Absoloader'
     setQuestionList(newQuestions);
     setQuiz({ ...quiz, questions: newQuestions });
   };
+
   const editQuestionSubmit = (question) => {
     setMode('add');
+
     const newData = questionList.map((item) => {
       if (question.quiz_id === item.quiz_id) {
         return question;
@@ -138,27 +166,33 @@ import Absoloader from '../../../shared/Loaders/Absoloader'
   useEffect(() => {
     if (mode === 'edit') {
       updateFields(edited);
+    }
   }, [mode, edited]);
+
   const setField = (type, e) => {
     const newData = { ...fields };
     newData[type] = e.target.value;
     updateFields(newData);
   };
+
   const setTrue = (type, e) => {
     const newData = { ...fields };
     newData[type] = e;
     updateFields(newData);
   };
+
   const setParent = (type, e) => {
     const newData = { ...quiz };
     newData[type] = e.target.value;
     setQuiz(newData);
   };
+
   const setCategory = (type, e) => {
     const newData = { ...quiz };
     newData[type] = e.target.value;
     setQuiz(newData);
   };
+
   const questionAction = () => {
     if (mode === 'edit') {
       editQuestionSubmit(fields);
@@ -174,6 +208,7 @@ import Absoloader from '../../../shared/Loaders/Absoloader'
       addQuestion(fields);
     }
   };
+
   return (
     <div style={{ display: 'flex' }}>
         {
@@ -229,6 +264,7 @@ import Absoloader from '../../../shared/Loaders/Absoloader'
             </>
           )}
         </div>
+
         {notification === true ? (
           <div className={styles.notification}>
             <div className={styles.nothead}>
@@ -240,12 +276,14 @@ import Absoloader from '../../../shared/Loaders/Absoloader'
                 <path d='M108.841 80.5999C111.906 82.1312 114.313 84.2504 123.166 93.2117C153.634 124.05 153.469 123.853 154.775 131.053C155.283 133.855 155.235 135.199 154.516 138.297C153.39 143.156 151.677 146.046 147.854 149.54C143.087 153.897 140.404 154.957 134.045 154.995C124.931 155.05 124.874 155.01 102.147 132.256C91.5449 121.642 82.3098 112.074 81.625 110.995C79.5546 107.729 78.5135 103.801 78.5141 99.2556C78.5141 95.7341 78.7895 94.4328 80.125 91.6428C82.4147 86.8587 86.4293 82.8503 91.3553 80.4293C95.3048 78.488 95.307 78.488 100.39 78.6282C104.505 78.7415 105.661 79.0113 108.841 80.5999Z' />
                 <path d='M86.9274 0.744438C92.8692 1.73335 97.6741 2.88717 100.95 4.1111C102.339 4.62996 103.979 5.20435 104.596 5.38777C107.117 6.1366 116.553 11.3807 120.262 14.0939C122.443 15.6903 126.336 19.0255 128.91 21.5054C144.972 36.9768 153.126 55.3168 153.11 75.9369C153.104 82.613 151.79 93.0804 150.589 96.0057C149.322 99.093 149.264 99.0447 123.899 73.7953C110.506 60.4627 98.6361 48.8785 97.5221 48.0517C94.2379 45.6161 87.4827 42.5366 83.5618 41.688C70.0099 38.7543 55.8505 44.4623 47.0165 56.4201C45.8296 58.0271 44.8581 59.6807 44.8581 60.0947C44.8581 60.5092 44.6393 61.0875 44.3717 61.3809C41.9154 64.0722 40.6276 78.6406 42.2672 85.1921C43.7104 90.9584 46.7158 96.2811 51.7597 102.002C54.2883 104.87 65.6173 116.58 76.9351 128.023C91.951 143.205 97.3611 149.012 96.9494 149.508C95.252 151.553 79.2646 153.333 69.9476 152.514C50.7006 150.823 33.8466 142.527 20.1404 127.998C10.313 117.58 3.24252 103.268 0.7767 88.8022C-0.591394 80.776 -0.0910456 66.0719 1.79759 58.8203C5.57317 44.3221 12.2128 32.6924 22.5422 22.4836C34.1023 11.0581 46.7848 4.27209 61.9662 1.39062C64.4343 0.921686 66.9585 0.431438 67.5755 0.300182C70.2399 -0.26579 82.555 0.0169195 86.9274 0.744438Z' />
               </svg>
+
               <p className={styles.notification_title}>{message.title}</p>
             </div>
             <div className={styles.notspace} />
             <div className={styles.notbody}>{message.description}</div>
           </div>
         ) : null}
+
         <div className={styles.right_wrapper}>
           {mode === 'edit' || mode === 'add' ? (
             <div className={styles.current_slide}>
@@ -261,6 +299,7 @@ import Absoloader from '../../../shared/Loaders/Absoloader'
                   value={fields.question}
                 />
               </div>
+
               <div className={styles.options_wrapper}>
                 <div
                   className={
@@ -429,6 +468,7 @@ import Absoloader from '../../../shared/Loaders/Absoloader'
                   </svg>
 
                   <p>{t('quiz.quizpublish')}</p>
+                </div>
               ),
             }[sidebarView]
           }
@@ -448,6 +488,7 @@ import Absoloader from '../../../shared/Loaders/Absoloader'
                 value={quiz.title}
               />
             </div>
+
             <div className={styles._form_wrapper}>
               <label className={styles._form_label_name} htmFor='l-quiz-name'>
                 {t('quiz.description')}
@@ -461,6 +502,7 @@ import Absoloader from '../../../shared/Loaders/Absoloader'
                 value={quiz.description}
               />
             </div>
+
             <div className={styles._form_wrapper}>
               <label className={styles._form_label_name} htmFor='price'>
                 Purchase Coins
@@ -474,6 +516,7 @@ import Absoloader from '../../../shared/Loaders/Absoloader'
                 value={quiz.purchaseCoins}
               />
             </div>
+
             <div className={styles._form_wrapper}>
               <label className={styles._form_label_name} htmFor='visibility'>
                 Visibility
@@ -487,6 +530,7 @@ import Absoloader from '../../../shared/Loaders/Absoloader'
                 <option value='public'>Public</option>
               </select>
             </div>
+
             <div className={styles._form_wrapper}>
               <label className={styles._form_label_name} htmFor='Difficulty'>
                 Difficulty
@@ -501,10 +545,12 @@ import Absoloader from '../../../shared/Loaders/Absoloader'
                 <option value='3'>Hard</option>
               </select>
             </div>
+
             <div className={styles._form_wrapper}>
               <label className={styles._form_label_name} htmFor='l-quiz-name'>
                 {t('quiz.category')}
               </label>
+
               <select
                 className={styles._form_input}
                 value={quiz.category}
@@ -571,6 +617,7 @@ import Absoloader from '../../../shared/Loaders/Absoloader'
                 onClick={() => checkValidation()}
                 className={styles.publish_cta}>
                 <p>{t('quiz.publish')}</p>
+
                 <svg
                   style={{ marginLeft: '.75em' }}
                   width='28'
@@ -586,10 +633,12 @@ import Absoloader from '../../../shared/Loaders/Absoloader'
                   />
                 </svg>
               </button>
+
               <button
                 onClick={() => setSidebarView('quiz_edit')}
                 className={styles.publish_cta}>
                 <p>{t('quiz.settings')}</p>
+
                 <svg
                   style={{ marginLeft: '.75em' }}
                   width='24'
@@ -611,6 +660,7 @@ import Absoloader from '../../../shared/Loaders/Absoloader'
                 </svg>
               </button>
             </div>
+
             <div className={styles.box_wrapper}>
               <div className={styles.boxes}>
                 <div className={styles.navicon}>
@@ -635,6 +685,7 @@ import Absoloader from '../../../shared/Loaders/Absoloader'
                   </p>
                 </div>
               </div>
+
               <div className={styles.boxes}>
                 <div className={styles.navicon}>
                   <svg
@@ -763,8 +814,15 @@ import Absoloader from '../../../shared/Loaders/Absoloader'
                   <h5>{item.answer2}</h5>
                   <h5>{item.answer3}</h5>
                   <h5>{item.answer4}</h5>
+                  <h5>{item.isCorrect}</h5>
                 </div>
               ))}
                 </div>
 
-                  <h5>{item.isCorrect}</h5>
+            </div>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+}
